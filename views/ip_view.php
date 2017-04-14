6fdd14e4061e954903e99b786399e48c54f729e6
nmish2005 <nmish2005@gmail.com>
2017-04-14 17:45:41 +0300

    count push

--- views/ip_view.php
+++ views/ip_view.php
@@ -1,7 +1,7 @@
 <?php
 include "start.php";
 $ip=$_SERVER['REMOTE_ADDR'];
-$insert="INSERT INTO `ip`(`ip_mysql`) VALUES ({$ip})";
+$insert="INSERT INTO `ip` (`ip_mysql`) VALUES ({$ip})";
 $count=count("SELECT `ip_mysql` FROM `ip` WHERE `id`!='0'");
 var_dump($insert);
 echo "<div class='alert alert-warning' role='alert'><span class='glyphicon glyphicon-console' aria-hidden='true'></span>Ваш IP-адрес -- ".$ip."</div><br>";
