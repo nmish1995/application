bc5ca79c9e56798f67d93510faeffa32e937e29d
nmish2005 <nmish2005@gmail.com>
2017-04-14 17:12:23 +0300

    count push

--- views/count_view.php
+++ views/count_view.php
@@ -1,13 +1,5 @@
 <?php
 
-$f=fopen("stat.dat","a+");
-flock($f,LOCK_EX);
-$count=fread($f,100);
-@$count++;
-ftruncate($f,0);
-fwrite($f,$count);
-fflush($f);
-flock($f,LOCK_UN);
-fclose($f);
+
 
 echo "Количество скачек/кликов: $count";
--- views/ip_view.php
+++ views/ip_view.php
@@ -1,3 +1,8 @@
 <?php
+
+include "start.php";
 $ip=$_SERVER['REMOTE_ADDR'];
-echo "<div class='alert alert-warning' role='alert'><span class='glyphicon glyphicon-console' aria-hidden='true'></span>Ваш IP-адрес -- ".$ip."</div>";
+$insert="INSERT INTO `ip`(`ip_mysql`) VALUES ([$ip])";
+$count=count("SELECT * FROM `ip`");
+echo "<div class='alert alert-warning' role='alert'><span class='glyphicon glyphicon-console' aria-hidden='true'></span>Ваш IP-адрес -- ".$ip."</div><br><br>";
+echo "<div class='alert alert-warning' role='alert'><span class='glyphicon glyphicon-console' aria-hidden='true'></span>Всего посещений -- ".$count."</div>";
