fdade0941fdbe359b5e9ffa230e22c0526d606ed
nmish2005 <nmish2005@gmail.com>
2017-04-15 22:49:08 +0300

    count push

--- views/ip_view.php
+++ views/ip_view.php
@@ -1,12 +1,17 @@
 <?php
 include "start.php";
-$ip=$_SERVER['REMOTE_ADDR'];
-$insert="INSERT INTO ip (`ip_mysql`) VALUES ({$ip})";
-$query = mysql_query($insert,$db);
-$result = mysql_query("SELECT `ip_mysql` FROM `ip`",$db);
-$data = mysql_fetch_array($result);
-echo count($data);
-var_dump($data);
-/*$count=count();*/
-echo "<div class='alert alert-warning' role='alert'><span class='glyphicon glyphicon-console' aria-hidden='true'></span>Ваш IP-адрес -- ".$ip."</div><br>";
-echo "<div class='alert alert-warning' role='alert'><span class='glyphicon glyphicon-console' aria-hidden='true'></span>Всего посещений -- ".$count."</div>";
+$ip = $_SERVER['REMOTE_ADDR'];
+$insert = "INSERT INTO ip (`ip_mysql`) VALUES ('{$ip}')";
+$ip_query = mysql_query("SELECT `ip_mysql` FROM `ip`",$db);
+$count = mysql_num_rows($ip_query);
+echo "<div class='alert alert-warning' role='alert'>
+<span class='glyphicon glyphicon-console' aria-hidden='true'>
+</span>
+Ваш IP-адрес -- ".$ip."
+</div>
+<br>
+<div class='alert alert-warning' role='alert'>
+<span class='glyphicon glyphicon-console' aria-hidden='true'>
+</span>
+Всего посещений -- ".$count."
+</div>";
