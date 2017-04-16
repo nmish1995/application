7be8870bcdae1c6bf7f2453fd879b4a020fab424
nmish2005 <nmish2005@gmail.com>
2017-04-16 23:16:32 +0300

    count push

--- models/Counter.php
+++ models/Counter.php
@@ -10,7 +10,7 @@ class Counter
 
     function saveVisit()
     {
-        $query=mysql_query("SELECT * FROM `counter` WHERE `ip`== 0 AND `controller`== 0 AND `action`== 0 AND `count`== 0");
+        $query=mysql_query("SELECT * FROM `counter` WHERE `ip` == 0 AND `controller` == 0 AND `action` == 0 AND `count` == 0");
         $num=mysql_num_rows($query);
         echo "TEST123";
     }
