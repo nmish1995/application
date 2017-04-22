8fd10e7d1c37a58dc330c9d99ff44dce41fdaf1c
nmish2005 <nmish2005@gmail.com>
2017-04-22 15:35:32 +0300

    count push

--- views/ip_view.php
+++ views/ip_view.php
@@ -3,7 +3,7 @@ include "start.php";
 $ip = $_SERVER['REMOTE_ADDR'];
 $mysql_ip=mysql_query("SELECT `ip` FROM `counter` WHERE `ip` == '{$ip}'");
 $count=mysql_query("SELECT SUM(count) FROM counter");
-var_dump($count);
+echo $count;
 if($mysql_ip != null){
     $count++;
     $query=mysql_query("UPDATE `counter` SET `count`='{$count}' WHERE `ip` == '{$ip}'");
