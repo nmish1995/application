ad0a413dc3b4e737a02372bdaec2209040069328
nmish2005 <nmish2005@gmail.com>
2017-04-23 11:06:08 +0300

    count push

--- views/ip_view.php
+++ views/ip_view.php
@@ -2,12 +2,13 @@
 include "start.php";
 $ip = $_SERVER['REMOTE_ADDR'];
 $mysql_ip=mysql_query("SELECT `ip` FROM `counter` WHERE `ip` == '{$ip}'");
+$num_rows=mysql_num_rows($mysql_ip);
 $sum=mysql_query("SELECT SUM(`count`) as 'sum' FROM `counter`");
 $count=mysql_fetch_assoc($sum);
-echo $count['sum'];
-if($mysql_ip != false){
+$count=$count['sum'];
+if($num_rows>0){
     $count++;
-    $query=mysql_query("UPDATE `counter` SET `count`='{$count}' WHERE `ip` == '{$ip}'");
+    $query=mysql_query("UPDATE `counter` SET `count`={$count} WHERE `ip` == '{$ip}'");
 }else{
     $first_count=1;
     $query=mysql_query("INSERT INTO counter (`ip`,`count`) VALUES ('{$ip}','{$first_count}')");
@@ -22,5 +23,5 @@ echo "<div class='alert alert-warning' role='alert'>
 <div class='alert alert-warning' role='alert'>
 <span class='glyphicon glyphicon-console' aria-hidden='true'>
 </span>
-Всего посещений -- ".$count['sum']."
+Всего посещений -- ".$count."
 </div>";
